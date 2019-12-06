b183e75b0317c1273580a08a6e916fd15006e8d9
jesus <jesus.tr61@gmail.com>
2019-12-06 14:56:12 -0500

    que raro

--- bembos/settings.py
+++ bembos/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(*oqz@tf2z^)mgr9i0@8k%8-^&fw)q&phm=uabf1nka&yd9=t9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
