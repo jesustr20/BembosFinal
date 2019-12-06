db57fed396ac73344a84c48dd84f9ae28e49f31f
jesus <jesus.tr61@gmail.com>
2019-12-06 14:42:30 -0500

    prueba

--- bembos/settings.py
+++ bembos/settings.py
@@ -130,9 +130,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = (
-##    os.path.join(BASE_DIR, 'static'),
-#    )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    )
 
 #STATICFILES_STORAGE = 'bembos.storage.CompressedManifestStaticFileStorage'
 
